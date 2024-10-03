d0574d04ec360664af710a025fc49aa4c1eb4aed
tranbinhminh2110 <tranbinhminh211002@gmail.com>
2024-10-04 00:01:29 +0700

    validator_major

---
+++ StudentCertificatePortal_API/Validators/CreateMajorRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using StudentCertificatePortal_API.Contracts.Requests;
+
+namespace StudentCertificatePortal_API.Validators
+{
+    public class CreateMajorRequestValidator: AbstractValidator<CreateMajorRequest>
+    {
+        public CreateMajorRequestValidator() 
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.MajorCode)
+                .NotEmpty().WithMessage("MajorCode is required.");
+            RuleFor(x => x.MajorName)
+                .NotEmpty().WithMessage("MajorName is required.");
+
+        }
+
+    }
+}
---
+++ StudentCertificatePortal_API/Validators/UpdateMajorRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using StudentCertificatePortal_API.Contracts.Requests;
+
+namespace StudentCertificatePortal_API.Validators
+{
+    public class UpdateMajorRequestValidator: AbstractValidator<UpdateMajorRequest>
+    {
+        public UpdateMajorRequestValidator() 
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.MajorCode)
+                .NotEmpty().WithMessage("MajorCode is required.");
+            RuleFor(x => x.MajorName)
+                .NotEmpty().WithMessage("MajorName is required.");
+        }
+    }
+}
