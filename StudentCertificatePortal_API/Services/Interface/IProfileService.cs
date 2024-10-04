f3f96a308141024942985ab19145d6cfc5d4c794
nguyenthanhdat1234 <datnguyen.010802@gmail.com>
2024-10-04 15:23:36 +0700

    commit stage 2

--- StudentCertificatePortal_API/Services/Interface/IProfileService.cs
+++ StudentCertificatePortal_API/Services/Interface/IProfileService.cs
@@ -6,7 +6,7 @@ namespace StudentCertificatePortal_API.Services.Interface
 {
     public interface IProfileService
     {
-        public async Task<UserDto> FindByEmailAsync(string email);
+        Task<UserDto> FindByEmailAsync(string email);
         Task<IdentityResult> ResetPasswordAsync(string email, string newPassword);
         Task<UserDto> GetProfileByIdAsync(int userId, CancellationToken cancellationToken);
         Task<UserDto> UpdateProfileAsync(int userId, UpdateProfileRequest request, CancellationToken cancellationToken);
