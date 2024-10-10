90c6441c10e48307e0e7074122370374df9143fa
nguyenthanhdat1234 <datnguyen.010802@gmail.com>
2024-10-10 15:59:51 +0700

    update database create answer table

---
+++ StudentCertificatePortal_Data/Models/Answer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentCertificatePortal_Data.Models;
+
+public partial class Answer
+{
+    public int AnswerId { get; set; }
+
+    public string Text { get; set; } = null!;
+
+    public bool IsCorrect { get; set; }
+
+    public int? QuestionId { get; set; }
+
+    public virtual Question? Question { get; set; }
+}
