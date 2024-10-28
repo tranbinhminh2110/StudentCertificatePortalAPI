73415ab0c5cbaf03c7add09d6c6392ee379dddb7
tranbinhminh2110 <tranbinhminh211002@gmail.com>
2024-10-28 16:38:21 +0700

    logic_course_permission

--- StudentCertificatePortal_API/Services/Interface/ICourseService.cs
+++ StudentCertificatePortal_API/Services/Interface/ICourseService.cs
@@ -1,5 +1,6 @@
 ﻿using StudentCertificatePortal_API.Contracts.Requests;
 using StudentCertificatePortal_API.DTOs;
+using StudentCertificatePortal_API.Enums;
 
 namespace StudentCertificatePortal_API.Services.Interface
 {
@@ -11,5 +12,7 @@ namespace StudentCertificatePortal_API.Services.Interface
         Task<CourseDto> UpdateCourseAsync(int courseId, UpdateCourseRequest request, CancellationToken cancellationToken);
         Task<CourseDto> DeleteCourseAsync(int courseId, CancellationToken cancellationToken);
         Task<List<CourseDto>> GetCourseByNameAsync(string courseName, CancellationToken cancellationToken);
+        Task<CourseDto> UpdateCoursePermissionAsync(int courseId, EnumPermission coursePermission, CancellationToken cancellationToken);
+
     }
 }
