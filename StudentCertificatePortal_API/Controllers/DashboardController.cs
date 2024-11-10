11419879c6dd7427dbbd83702ff7b1a411827d6d
nguyenthanhdat1234 <datnguyen.010802@gmail.com>
2024-11-10 13:18:29 +0700

    get summary dashboard

---
+++ StudentCertificatePortal_API/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentCertificatePortal_API.Commons;
+using StudentCertificatePortal_API.DTOs;
+using StudentCertificatePortal_API.Services.Implemetation;
+using StudentCertificatePortal_API.Services.Interface;
+
+namespace StudentCertificatePortal_API.Controllers
+{
+    public class DashboardController : ApiControllerBase
+    {
+        private readonly IDashboardService _service;
+        public DashboardController(IDashboardService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("summary")]
+        public async Task<ActionResult<DashboardSummaryDto>> GetDashboardSummary()
+        {
+            var summary = await _service.GetDashboardSummaryAsync(new CancellationToken());
+            return Ok(Result<DashboardSummaryDto>.Succeed(summary));
+        }
+    }
+}
