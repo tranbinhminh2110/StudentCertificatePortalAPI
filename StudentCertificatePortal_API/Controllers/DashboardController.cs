a503e27371e189913ad00978edaeb3c9e264a1a9
nguyenthanhdat1234 <datnguyen.010802@gmail.com>
2024-11-10 14:12:40 +0700

    add totalPoint by time

--- StudentCertificatePortal_API/Controllers/DashboardController.cs
+++ StudentCertificatePortal_API/Controllers/DashboardController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StudentCertificatePortal_API.Commons;
 using StudentCertificatePortal_API.DTOs;
+using StudentCertificatePortal_API.Enums;
 using StudentCertificatePortal_API.Services.Implemetation;
 using StudentCertificatePortal_API.Services.Interface;
 
@@ -20,5 +21,25 @@ namespace StudentCertificatePortal_API.Controllers
             var summary = await _service.GetDashboardSummaryAsync(new CancellationToken());
             return Ok(Result<DashboardSummaryDto>.Succeed(summary));
         }
-    }
-}
+
+        [HttpGet("total-point")]
+        public async Task<IActionResult> GetTotalAmount(
+            [FromQuery] TimePeriod period,
+            [FromQuery] DateTime? startDate = null,
+            [FromQuery] DateTime? endDate = null)
+        {
+            try
+            {
+                decimal totalPoint = await _service.GetTotalAmountAsync(period, startDate, endDate);
+                return Ok(Result<decimal>.Succeed(totalPoint));
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An unexpected error occurred.", details = ex.Message });
+            }
+        }
+}}
