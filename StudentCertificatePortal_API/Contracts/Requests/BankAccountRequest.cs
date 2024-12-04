6ddd452b48fdeb1ff54e919de1e8ef869cdd046f
nguyenthanhdat1234 <datnguyen.010802@gmail.com>
2024-12-04 12:30:07 +0700

    update email for refund request

--- StudentCertificatePortal_API/Contracts/Requests/BankAccountRequest.cs
+++ StudentCertificatePortal_API/Contracts/Requests/BankAccountRequest.cs
@@ -4,12 +4,15 @@ namespace StudentCertificatePortal_API.Contracts.Requests
 {
     public class BankAccountRequest
     {
+        [Required(ErrorMessage = "Bank name is required.")]
+        [StringLength(100, ErrorMessage = "Bank name cannot exceed 100 characters.")]
+        public string BankName { get; set; }
+
+
         [Required(ErrorMessage = "Account number is required.")]
         [StringLength(20, ErrorMessage = "Account number cannot exceed 20 characters.")]
         public string AccountNumber { get; set; }
 
-        [Required(ErrorMessage = "Bank name is required.")]
-        [StringLength(100, ErrorMessage = "Bank name cannot exceed 100 characters.")]
-        public string BankName { get; set; }
+        
     }
 }
