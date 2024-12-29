a3be2cb9e2756cec8f841c9f0876b29e5fffbb55
tranbinhminh2110 <tranbinhminh211002@gmail.com>
2024-12-30 00:09:19 +0700

    voucher_exam_enrollment

--- StudentCertificatePortal_API/Contracts/Requests/UpdateExamEnrollmentVoucherRequest.cs
+++ StudentCertificatePortal_API/Contracts/Requests/UpdateExamEnrollmentVoucherRequest.cs
@@ -5,6 +5,6 @@
         public int? UserId { get; set; }
 
         public List<int> Simulation_Exams { get; set; } = new List<int>();
-        public int? VoucherId { get; set; }
+        public List<int> VoucherIds { get; set; } = new List<int>();
     }
 }
