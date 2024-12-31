5c829f9a76b829ee2ab8b5d080736901289db62f
datnguyen.010802@gmail.com <Khongnhonho@23>
2024-12-31 16:08:20 +0700

    update peerReview

---
+++ StudentCertificatePortal_Repository/Implementation/PeerReviewDetailRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentCertificatePortal_Data.Models;
+
+namespace StudentCertificatePortal_Repository.Implementation
+{
+    public class PeerReviewDetailRepository : BaseRepository<PeerReviewDetail>
+
+    {
+        public PeerReviewDetailRepository(CipdbContext context) : base(context) { }
+    }
+}
