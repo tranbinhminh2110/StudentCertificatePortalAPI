da81cc67daab5aaba04eac173efe86c0d764b14a
nguyenthanhdat1234 <datnguyen.010802@gmail.com>
2025-01-07 10:40:13 +0700

    update second

--- StudentCertificatePortal_API/Services/Implemetation/PeerReviewService.cs
+++ StudentCertificatePortal_API/Services/Implemetation/PeerReviewService.cs
@@ -353,18 +353,21 @@ namespace StudentCertificatePortal_API.Services.Implemetation
             if (reviewedUser != null)
             {
                 var emailSubject = "Peer Review Process Completed";
-                var emailBody = $@"Dear {reviewedUser.Fullname},
+                var emailBody = $@"Dear {reviewedUser.Fullname},<br><br>
 
-We are pleased to inform you that your submission ({exam.ExamName}) has been successfully reviewed as part of the peer review process.
+We are pleased to inform you that your submission (<b>{exam.ExamName}</b>) has been successfully reviewed as part of the peer review process.<br><br>
 
-Review Date: {peerReview.ReviewDate}
+<b>Reviewer:</b> {peerReview.Reviewer?.Fullname}<br><br>
 
-We sincerely appreciate your efforts and contributions to this initiative. Should you have any further inquiries or require additional information, please do not hesitate to reach out.
+<b>Review Date:</b> {peerReview.ReviewDate.ToString("dd/MM/yyyy")}<br><br>
 
-Thank you for your participation and dedication.
+We sincerely appreciate your efforts and contributions to this initiative. Should you have any further inquiries or require additional information, please do not hesitate to reach out.<br><br>
 
-Best regards,  
-Student Information Portal";
+Thank you for your participation and dedication.<br><br>
+
+Best regards,<br>  
+<b>Student Information Portal</b>";
+
 
 
                 await _emailService.SendEmailAsync(reviewedUser.Email, emailSubject, emailBody);
